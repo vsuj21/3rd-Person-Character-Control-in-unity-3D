4cf9c4b3e7b037ba8f8bacfd258ee19f26d7b43c
vsuj21 <86345373+vsuj21@users.noreply.github.com>
2023-04-17 21:08:50 +0530

    Update Player_Loco_Motion.cs

--- Player_Loco_Motion.cs
+++ Player_Loco_Motion.cs
@@ -8,8 +8,14 @@ public class Player_Loco_Motion : MonoBehaviour
     Vector3 moveDirection;
     Transform CameraObject;
     Rigidbody Player_rigid_body;
-    public float movespeed = 9;
-    public float rotationspeed = 7;
+
+    public bool Isrun;
+
+    [Header("Movement speeds")]
+
+    public float rotationspeed = 4;
+    public float walkingspeed = 3;
+    public float sprintingspeed =10;
 
     private void Awake()
     {
@@ -23,13 +29,23 @@ public class Player_Loco_Motion : MonoBehaviour
         HandleMovement();
         HandleRotation();
     }
+
     private void HandleMovement()
     {
         moveDirection = CameraObject.forward * Input_Manager.VerticalInput;
         moveDirection = moveDirection + CameraObject.right * Input_Manager.HorizontalInput;
         moveDirection.Normalize();
         moveDirection.y = 0;
-        moveDirection = moveDirection * movespeed;
+        if (Isrun)
+        {
+            moveDirection = moveDirection * sprintingspeed;
+        }
+        else
+        {
+            moveDirection = moveDirection * walkingspeed;
+        }
+        
+        
 
         Vector3 movementVelocity = moveDirection;
         Player_rigid_body.velocity = movementVelocity;
